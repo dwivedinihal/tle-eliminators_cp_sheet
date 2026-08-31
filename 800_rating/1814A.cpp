5d091203349ff690d38be1fe217d7dc8080cec94
dwivedinihal <dwivedinihal005@gmail.com>
2026-09-01 00:57:04 +0530

    Update 1814A.cpp

--- 800_rating/1814A.cpp
+++ 800_rating/1814A.cpp
@@ -6,11 +6,10 @@ int main(){
      cin >> t;
 
      while(t--){
-          int n,k;
+          long long n,k;
           cin >> n >> k;
 
-          if(n % 2 == 0) cout << "YES\n";
-          else if(n % 2 != 0 && k % 2 != 0) cout << "YES\n";
-          else cout << "NO\n";
+          if(n % 2 != 0 && k % 2 == 0) cout << "NO\n";
+          else cout << "YES\n";
      }
 }
