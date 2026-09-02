c21597b897d0cc51a05e2dd1a2c2f48f864827f1
dwivedinihal <dwivedinihal005@gmail.com>
2026-09-02 08:52:34 +0530

    Create 1761A.cpp

---
+++ 800_rating/1761A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int t;
+     cin >> t;
+
+     while(t--){
+          int n,a,b;
+          cin >> n >> a >> b;
+
+          if((a == n && b == n) || ((a+b) <= n-2)) cout << "Yes\n";
+          else cout << "No\n";
+     }
+}
